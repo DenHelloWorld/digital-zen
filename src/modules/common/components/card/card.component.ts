201728faeb59759a6dc421af38a851527f328d5f
denhelloworld <denissaveliev23@gmail.com>
2025-09-12 06:19:49 +0300

    refactor(app): use sprite icon strategy

---
+++ src/modules/common/components/card/card.component.ts
@@ -0,0 +1,9 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  selector: 'dz-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CardComponent {}
