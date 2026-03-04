ca288f5e38ee0d26ba3ffcb1f8497a9c63a4281a
denhelloworld <denissaveliev23@gmail.com>
2026-03-04 02:16:26 +0200

    feat(core): save current route

--- src/modules/common/services/mini-router.service.ts
+++ src/modules/common/services/mini-router.service.ts
@@ -1,8 +1,14 @@
+import { CHROME_STORAGE_KEY_ENUM } from '../enums/chrome-storage-key.enum';
 import { VIEW_ENUM, ViewType } from '../enums/view.enum';
-import { Injectable, signal } from '@angular/core';
+import { ChromeStorageService } from './chrome-storage.service';
+import { inject, Injectable, signal } from '@angular/core';
+import { takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
+import { distinctUntilChanged } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class MiniRouterService {
+  readonly #storageService = inject(ChromeStorageService);
+
   readonly #currentRoute = signal<ViewType>(VIEW_ENUM.FOCUS);
   readonly #payload = signal<unknown | null>(null);
   readonly #isSidePanel = signal<boolean>(window.location.search.includes('view=sidepanel'));
@@ -13,6 +19,20 @@ export class MiniRouterService {
   public readonly previousRoute = this.#previousRoute.asReadonly();
 
   public readonly isSidePanel = this.#isSidePanel.asReadonly();
+
+  constructor() {
+    this.#storageService.get<ViewType>(CHROME_STORAGE_KEY_ENUM.CURRENT_ROUTE, route => {
+      if (route) {
+        this.#currentRoute.set(route);
+      }
+    });
+
+    toObservable(this.#currentRoute)
+      .pipe(distinctUntilChanged(), takeUntilDestroyed())
+      .subscribe(route => {
+        this.#storageService.set(CHROME_STORAGE_KEY_ENUM.CURRENT_ROUTE, route);
+      });
+  }
 
   public navigate(route: ViewType, payload: object | null = null) {
     this.#previousRoute.set(this.#currentRoute());
